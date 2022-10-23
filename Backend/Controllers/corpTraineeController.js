64f68c52cad34758553e6eed67fca1651fcfb9bb
Misk <miskmabdalla@gmail.com>
2022-10-23 23:30:02 +0200

    added corptrainee and instructor

---
+++ Backend/Controllers/corpTraineeController.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const CorpTrainee = require('../Models/corpTraineeModel');
+
+const newCTrainee = (req, res) => {
+        corpTrainee = new CorpTrainee({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.firstName + req.body.lastName,
+        password: Math.floor(Math.random() * 100000000),
+        country: req.body.country,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address
+    })
+    corpTrainee.save()
+        .then(result => {
+            res.status(200).json({
+                message: "Corp Trainee added successfully",
+                corpTrainee: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Corp Trainee could not be added",
+                error: err
+            })
+        })
+}
+module.exports = {
+    newCTrainee
+}
--- Backend/Controllers/instructorController.js
+++ Backend/Controllers/instructorController.js
@@ -8,18 +8,33 @@ const Instructor = require('../Models/instructorModel');
 // get a single instructor
 
 // post a new instructor
-const postInstructor = async(req, res) => {
-    const { userName, password, country } = req.body;
-    try {
-        const instructor = await Instructor.create({ userName, password, country });
-        res.status(200).json(instructor);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+const newInstructor = (req, res) => {
+    instructor = new Instructor({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    userName: req.body.firstName + req.body.lastName,
+    password: Math.floor(Math.random() * 100000000),
+    country: req.body.country,
+    phoneNumber: req.body.phoneNumber,
+    address: req.body.address
+})
+instructor.save()
+    .then(result => {
+        res.status(200).json({
+            message: "Instructor added successfully",
+            instructor: result
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Instructor could not be added",
+            error: err
+        })
+    })
 }
-
-
-
+module.exports = {
+    newInstructor
+}
 // delete an instructor
 
 // update an instructor
