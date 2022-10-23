fa323d2ffac0384396b08ad7201c75241adcc7df
Khaled Ayman <khaledayman012@gmail.com>
2022-10-23 23:21:44 +0200

    updated Controllers and Routes Files, added Frontend

--- Backend/Controllers/instructorController.js
+++ Backend/Controllers/instructorController.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const Instructor = require('../Models/instructorModel');
-
+const mongoose = require('mongoose');
 
 // get all instructors
-
+const getInstructors = async(req, res) => {
+    const instructors = await Instructor.find({}).sort({ createdAt: -1 });
+    res.status(200).json(instructors);
+}
 
 // get a single instructor
+const getInstructor = async(req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'There does not exist an instructor with the corresponding id.' });
+    }
+    const instructor = await Instructor.findById(id)
+    if (!instructor) {
+        return res.status(404).json({ error: 'No such instructor' });
+    }
+    res.status(200).json(instructor);
+}
 
 // post a new instructor
 const postInstructor = async(req, res) => {
@@ -21,12 +35,38 @@ const postInstructor = async(req, res) => {
 
 
 // delete an instructor
-
-// update an instructor
-
+const deleteInstructor = async(req, res) => {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'There does not exist an instructor with the corresponding id.' });
+        }
+        const instructor = await Instructor.findOneAndDelete({ _id: id });
+        if (!instructor) {
+            return res.status(404).json({ error: 'No such instructor' });
+        }
+        res.status(200).json(instructor);
+    }
+    // update an instructor
+const updateInstructor = async(req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'There does not exist an instructor with the corresponding id.' });
+    }
+    const instructor = await Instructor.findByIdAndUpdate({ _id: id }, {
+        ...req.body
+    });
+    if (!instructor) {
+        return res.status(404).json({ error: 'No such instructor' });
+    }
+    res.status(200).json(instructor);
+}
 
 
 //export the functions
 module.exports = {
-    postInstructor
+    getInstructors,
+    getInstructor,
+    postInstructor,
+    deleteInstructor,
+    updateInstructor
 }
