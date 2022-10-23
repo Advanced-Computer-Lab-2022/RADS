a5e50afc2913d11723bcbb380488334c627fd004
Misk <miskmabdalla@gmail.com>
2022-10-23 21:58:00 +0200

    added to model files

--- Backend/Models/corpTraineeModel.js
+++ Backend/Models/corpTraineeModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const instructorSchema = new Schema({
+const corpTraineeSchema = new Schema({
     userName: {
         type: String,
         required: true
@@ -22,11 +22,9 @@ const instructorSchema = new Schema({
     address: {
         type: String,
         required:true
-    }n
+    }
 
 }, { timestamps: true })
 
 
-module.exports = mongoose.model('Instructor', instructorSchema);
-
-//Instructor.find()
+module.exports = mongoose.model('corpTrainee', corpTraineeSchema);
