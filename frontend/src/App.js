6bf58b1a40a5d9ecc934d3a6c5af1d311b18ba3b
Khaled Ayman <khaledayman012@gmail.com>
2022-10-25 15:02:46 +0200

    added new webpages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './webcomponents/Navbar';
+import Home from './webpages/Home';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Navbar />
+        <div className='webpages'>
+          <Routes>
+            <Route
+            path="/"
+            element={<Home/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/webpages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Home page</h2>
+        </div>
+    )
+}
+
+
+export default Home
