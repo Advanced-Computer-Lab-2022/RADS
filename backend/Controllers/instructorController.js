533b44b17a170f1bdaf856ead0f0e173c3da4eb7
Khaled Ayman <khaledayman012@gmail.com>
2022-10-26 01:51:07 +0200

    Updated the postInstructor method

--- backend/Controllers/instructorController.js
+++ backend/Controllers/instructorController.js
@@ -21,27 +21,34 @@ const getInstructor = async(req, res) => {
 }
 
 //post new instructor
-const postInstructor = async (req, res) => {
-    const {firstName, lastName, country, phoneNumber, address} = req.body;
-      try {
-      const instructor = await Instructor.create({ firstName, lastName, country, phoneNumber, address, userName: req.body.firstName + req.body.lastName,
-        password: Math.floor(Math.random() * 100000000) });
-      res.status(200).json({ message: "Instructor added successfully", message: "Your username is " + instructor.userName + " and your password is " + instructor.password});
+const postInstructor = async(req, res) => {
+    const { firstName, lastName, password, country, phoneNumber, address } = req.body;
+    try {
+        const instructor = await Instructor.create({
+            firstName,
+            lastName,
+            userName: req.body.firstName + req.body.lastName,
+            password,
+            country,
+            phoneNumber,
+            address
+        });
+        res.status(200).json({ message: "Instructor added successfully", message: "Your username is " + instructor.userName + " and your password is " + instructor.password });
     } catch (error) {
-      res.status(400).json({ error: error.message });
+        res.status(400).json({ error: error.message });
     }
-  }
+}
 
 // DELETE an instructor
 const deleteInstructor = async(req, res) => {
     const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: 'There does not exist an instructor with the corresponding id.' });
-        }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'There does not exist an instructor with the corresponding id.' });
+    }
     const instructor = await Instructor.findOneAndDelete({ _id: id });
-        if (!instructor) {
-            return res.status(404).json({ error: 'No such instructor' });
-        }
+    if (!instructor) {
+        return res.status(404).json({ error: 'No such instructor' });
+    }
     res.status(200).json(instructor);
 }
 
