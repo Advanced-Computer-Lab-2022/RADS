f589ca3a57f844e0861e9858d6310b7a22fdbee8
Khaled Ayman <khaledayman012@gmail.com>
2022-10-25 22:39:39 +0200

    Updating the home page

--- frontend/src/webpages/Home.js
+++ frontend/src/webpages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect,useState } from "react"
+
+// Components
+import InstructorDetails from '../webcomponents/InstructorDetails'
+
+// To fetch all instructor from the backend
 const Home = () => {
+    const [instructors, setInstructors] = useState(null);
+    useEffect(()=>{
+        const fetchInstructors = async () => {
+            const response = await fetch('/Instructor');
+            const json = await response.json();
+
+            if(response.ok){
+                setInstructors(json)
+            }
+        }
+
+        fetchInstructors();
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home page</h2>
+            <div className="instructors">
+                {instructors && instructors.map((instructor)=>(
+                    // <p key = {instructor._id}>{instructor.userName}</p>
+                    <InstructorDetails key = {instructor._id} instructor={instructor}/>
+                ))}
+            </div>
         </div>
     )
 }
