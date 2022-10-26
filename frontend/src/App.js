3e9dd6f83116156bc1d5fac45683311eb3d2d51f
1hassanharidy <1hassanharidy@gmail.com>
2022-10-27 01:30:56 +0200

    added a test button usin MaterialUI in App.js frnt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,22 @@ import{BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './webcomponents/Navbar';
 import Home from './webpages/Home';
 import InstructorLobby from './webpages/InstructorLobby';
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+
+        <Button variant="contained">Dummy Button</Button>
+        
         <div className='webpages'>
           <Routes>
             <Route
