4f9fad9f99583b0b13678b581a714579415d640e
Khaled Ayman <khaledayman012@gmail.com>
2022-10-26 17:00:29 +0200

    added instructor of the course to the schema

--- backend/Models/courseModel.js
+++ backend/Models/courseModel.js
@@ -19,6 +19,9 @@ const courseSchema = new Schema({
     shortSummary: {
         type: String,
         required: true
+    },
+    instructor: {
+        type: String
     }
 
 }, { timestamps: true })
