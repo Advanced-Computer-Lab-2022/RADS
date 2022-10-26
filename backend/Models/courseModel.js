2b5a4755f207abb5d9bee9e1a24ba75c9876f56d
khaled ayman <khaledayman012@gmail.com>
2022-10-26 12:28:50 +0200

    Added the Course model,controller and route

---
+++ backend/Models/courseModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+    courseTitle: {
+        type: String,
+        required: true
+    },
+    subtitles: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    shortSummary: {
+        type: Number,
+        required: true
+    }
+
+}, { timestamps: true })
+
+//module.exports = mongoose.model('corpTrainee', corpTraineeSchema);
