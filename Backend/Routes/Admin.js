baf6ac8640516b259d5edaaaecd36384d36686d7
Khaled Ayman <105018459+Khaledayman9@users.noreply.github.com>
2022-10-26 17:54:36 +0200

    old backend

--- Backend/Routes/Admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { postAdmin } = require('../Controllers/adminController');
-
-const router = express.Router();
-
-router.post('/add', postAdmin);
-
-module.exports = router;
