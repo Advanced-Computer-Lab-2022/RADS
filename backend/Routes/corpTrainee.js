50c81c66fe477e7d177d0230451b0ab1e14157ed
Ahmed Khaled <30718750+ahmillect@users.noreply.github.com>
2022-10-27 23:38:22 +0200

    reuploading

--- backend/Routes/corpTrainee.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { postCTrainee } = require('../Controllers/adminController');
-const { getCTrainees } = require('../Controllers/corpTraineeController');
-const router = express.Router();
-
-router.post('/add', postCTrainee);
-router.get('/', getCTrainees);
-
-module.exports = router;
