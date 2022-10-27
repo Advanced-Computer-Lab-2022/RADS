a57f90ff8008e64b570b226a51bd0d4464b9ebaf
Ahmed Khaled <eng.ahmed.khaled15@gmail.com>
2022-10-27 23:36:11 +0200

    typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,8 @@ import{BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './webcomponents/Navbar';
 import Home from './webpages/Home';
 import InstructorLobby from './webpages/InstructorLobby';
-<<<<<<< HEAD
-
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-=======
 import Admin from './webpages/Admin';
->>>>>>> 19fa4e3e073dee7b4059fd9ba090f72d7fbbcc3e
+
 function App() {
   return (
     <div className="App">
