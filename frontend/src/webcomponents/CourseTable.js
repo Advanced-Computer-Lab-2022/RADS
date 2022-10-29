2ea3a842449001ace069ed7eccde77d7ff1fa2c9
khaled ayman <khaledayman012@gmail.com>
2022-10-29 05:02:50 +0200

    Created a table to show all courses in the DB

---
+++ frontend/src/webcomponents/CourseTable.js
@@ -0,0 +1,30 @@
+const CourseTable = ({data}) => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>Course Title</th>
+                    <th>Subtitles</th>
+                    <th>Price</th>
+                    <th>Short summary</th>
+                    <th>Subject</th>
+                    <th>Instructor of the course</th>
+                    <th>Rating</th>
+                </tr>
+                {data.map(item=>(
+                <tr key ={item._id}>
+                    <td>{item.courseTitle}</td>
+                    <td>{item.subtitles}</td>
+                    <td>{item.price}</td>
+                    <td>{item.shortSummary}</td>
+                    <td>{item.subject}</td>
+                    <td>{item.instructor}</td>
+                    <td>{item.rating}</td>
+                </tr>    
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default CourseTable;
