849e43b26b9616875cdebd3ce294ab35c2a7b588
khaled ayman <khaledayman012@gmail.com>
2022-10-29 05:06:28 +0200

    Updated the functionality of the postCourse function

--- backend/Controllers/courseController.js
+++ backend/Controllers/courseController.js
@@ -22,13 +22,15 @@ const getCourse = async(req, res) => {
 
 // POST new course
 const postCourse = async(req, res) => {
-    const { courseTitle, subtitles, price, shortSummary } = req.body;
+    const { courseTitle, subtitles, price, shortSummary, subject, instructor } = req.body;
     try {
         const course = await Course.create({
             courseTitle,
             subtitles,
             price,
             shortSummary,
+            subject,
+            instructor
         });
         res.status(200).json({ message: "Course added successfully", message: "Course info" + course });
     } catch (error) {
