122384b8e66ee4ce06d2d23549dcc7e1699974be
AliSerafy <alielserafy22@gmail.com>
2022-10-29 11:30:43 +0200

    commit

--- frontend/src/webpages/InstructorProfile.js
+++ frontend/src/webpages/InstructorProfile.js
@@ -3,7 +3,7 @@ import { useEffect,useState } from "react"
 // Components
 import InstructorProfile from "../webcomponents/InstructorProfile";
 
-// To fetch all instructor from the backend
+// To fetch an instructor and view the courses and details from the backend
 const InstructorProfile = () => {
     return (
         <div className="instructor-profile">
