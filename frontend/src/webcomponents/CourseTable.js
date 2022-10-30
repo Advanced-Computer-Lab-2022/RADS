51d53f2e838bac6f5fd5a9f07dcf890f79eac06c
khaled ayman <khaledayman012@gmail.com>
2022-10-30 04:21:41 +0200

    Adjusted the layout of the table

--- frontend/src/webcomponents/CourseTable.js
+++ frontend/src/webcomponents/CourseTable.js
@@ -8,8 +8,10 @@ const CourseTable = ({data}) => {
                     <th>Price</th>
                     <th>Short summary</th>
                     <th>Subject</th>
+                    <th>Total hours</th>
                     <th>Instructor of the course</th>
                     <th>Rating</th>
+                    <th>Course exercises</th>
                 </tr>
                 {data.map(item=>(
                 <tr key ={item._id}>
@@ -18,8 +20,10 @@ const CourseTable = ({data}) => {
                     <td>{item.price}</td>
                     <td>{item.shortSummary}</td>
                     <td>{item.subject}</td>
+                    <td>{item.totalHours}</td>
                     <td>{item.instructor}</td>
-                    <td>{item.rating}</td>
+                    <td>{item.courseRating}</td>
+                    <td>{item.courseExercises}</td>
                 </tr>    
                 ))}
             </tbody>
