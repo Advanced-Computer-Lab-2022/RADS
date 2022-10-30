0785349b6d21ff5bddd6539279683d0a170fbe26
Khaled Ayman <105018459+Khaledayman9@users.noreply.github.com>
2022-10-30 23:01:40 +0200

    old stuff

--- backend/Routes/corpTrainee.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getCTrainees } = require('../Controllers/corpTraineeController');
-const router = express.Router();
-
-router.get('/', getCTrainees);
-
-module.exports = router;
