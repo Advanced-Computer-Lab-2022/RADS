465e9bd250c96d580661e548fb29cc0eb3dd3440
khaled ayman <khaledayman012@gmail.com>
2022-10-30 03:01:59 +0200

    Updated the Home Layout Page

--- frontend/src/webpages/Home.js
+++ frontend/src/webpages/Home.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
+import CourseDetails from '../webcomponents/CourseDetails';
+
 const Home = () => {
     const [courses, setCourses] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
 useEffect(() => {
     const fetchCourses = async () => {
         const response = await fetch('/course');
@@ -8,20 +11,36 @@ useEffect(() => {
 
         if(response.ok){
             setCourses(json)
+            setOpenModal(false);
             
         }
     }
     fetchCourses();
 }, [])
 
+
+    // const renderDetails = (key,course) =>{
+    //     console.log(course)
+    //     return (  
+  
+    //         <CourseDetails key={key} course = {course} />
+          
+    //     )
+
+    // }
     return (
         <div className="home-lobby">
             Welcome to RADS Online Course Provider
             <div className="instructors">
                 {courses && courses.map((course)=>(
+                     <div>
                      <p key = {course._id}>Course: {course.courseTitle}, Total Hours: {course.totalHours}
                      ,Rating = {course.rating} Out of 5</p>
+                     <button  className={course._id} onClick={()=>{setOpenModal(true);}}>View Course</button>
+                    {openModal && <CourseDetails key={course._id} course = {course} />}
+                     </div>
                 ))}
+                
             </div>
         </div>
     )
