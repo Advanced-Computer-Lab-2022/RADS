24dbc2d3fae75ae82ab485e0e3c615bccd79b9a9
ali elserafy <alielserafy22@gmail.com>
2022-12-02 00:56:08 +0200

    pulling

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -4,29 +4,47 @@ const Instructor = require('../Models/instructorModel');
 const CorpTrainee = require('../Models/corpTraineeModel');
 const mongoose = require('mongoose');
 
-const postAdmin = async (req, res) => {
-      const admin = await Admin.create({ userName: randomstring.generate(7), password: randomstring.generate(7) });
-      res.status(200).json({ message: "Admin added successfully", message: "Your username is " + admin.userName + " and your password is " + admin.password});
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  }
-  const postInstructor = async (req, res) => {
-    const {firstName, lastName, country, phoneNumber, address} = req.body;
+const postAdmin = async(req, res) => {
+    const { firstName, lastName } = req.body;
       try {
-      const instructor = await Instructor.create({ firstName, lastName, country, phoneNumber, address, userName: req.body.firstName + req.body.lastName,
-        password: randomstring.generate(7) });
-      res.status(200).json({ message: "Instructor added successfully", message: "Your username is " + instructor.userName + " and your password is " + instructor.password});
+        const admin = await Admin.create({ firstName, lastName, userName: req.body.firstName + req.body.lastName, password: randomstring.generate(7) });
+        res.status(200).json({ message: "Admin added successfully", message: "Your username is " + admin.userName + " and your password is " + admin.password });
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
   }
-const postCTrainee = async (req, res) => {
-  const {firstName, lastName, country, phoneNumber, address} = req.body;
+const postInstructor = async(req, res) => {
+    const { firstName, lastName, country, phoneNumber, address, rating, email, bio } = req.body;
+      try {
+        const instructor = await Instructor.create({
+            firstName,
+            lastName,
+            country,
+            phoneNumber,
+            address,
+            userName: req.body.firstName + req.body.lastName,
+            password: randomstring.generate(7),
+            email,
+            bio
+        });
+        res.status(200).json({ message: "Instructor added successfully", message: "Your username is " + instructor.userName + " and your password is " + instructor.password });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+const postCTrainee = async(req, res) => {
+    const { firstName, lastName, country, phoneNumber, address } = req.body;
     try {
-    const corpTrainee = await CorpTrainee.create({ firstName, lastName, country, phoneNumber, address, userName: req.body.firstName + req.body.lastName,
-      password: randomstring.generate(7) });
-    res.status(200).json({ message: "Corporate trainee added successfully", message: "Your username is " + corpTrainee.userName + " and your password is " + corpTrainee.password});
+        const corpTrainee = await CorpTrainee.create({
+            firstName,
+            lastName,
+            country,
+            phoneNumber,
+            address,
+            userName: req.body.firstName + req.body.lastName,
+            password: randomstring.generate(7)
+        });
+        res.status(200).json({ message: "Corporate trainee added successfully", message: "Your username is " + corpTrainee.userName + " and your password is " + corpTrainee.password });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/Routes/Instructor.js
+++ backend/Routes/Instructor.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const { getInstructor, getInstructors, deleteInstructor, updateInstructor, viewCourses } = require('../Controllers/instructorController');
+const { getInstructor, getInstructors, deleteInstructor, updateInstructor, filterCourses, postInstructor } = require('../Controllers/instructorController');
 const router = express.Router();
 
 // GET all instructors info
 router.get('/', getInstructors);
 
 // GET a single instructor's info 
-router.get('/:id', getInstructor)
+router.get('/:id', getInstructor);
+
+// Post an instructor
+router.get('/add', postInstructor);
 
 // GET all courses the instructors give
-router.get('/viewcourses/:id', viewCourses);
-
-
+router.get('/filter', filterCourses);
 
 // DELETE an instructor
 router.delete('/:id', deleteInstructor);
 
-router.patch('/:id', updateInstructor);
+router.patch('/changeInfo/:id', updateInstructor);
 
 
 
