e22c9352a8d7d0b2892c509f5f1a0552912c24f6
ali elserafy <alielserafy22@gmail.com>
2022-12-02 00:59:40 +0200

    pull

--- frontend/src/webpages/InstructorLobby.js
+++ frontend/src/webpages/InstructorLobby.js
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react"
 // Components
 import CourseCreate from "../webcomponents/CourseCreate"
 import SearchCourse from "../webcomponents/SearchCourse"
-import InstructorDetails from '../webcomponents/InstructorDetails'
-import ViewProfileButton from "../webcomponents/ViewProfileButton";
-const InstructorLobby = () => {
+
+const InstructorLobby = (props) => {
+    const{
+        rateVal,
+        currencyVal
+    } = props;
     const [instructors, setInstructors] = useState(null);
+    const [instructorsinfo, changeInfo] = useState(null);
     useEffect(() => {
         const fetchInstructors = async () => {
             const response = await fetch('/Instructor');
@@ -18,16 +22,30 @@ const InstructorLobby = () => {
         }
         fetchInstructors();
     }, [])
+    useEffect(() => {
+        const fetchInstructors = async () => {
+            const response = await fetch('/Instructor/changeInfo/:id');
+            const json = await response.json();
+    
+            if(response.ok){
+                changeInfo(json)
+                
+                
+            }
+        }
+        fetchInstructors();
+    }, [])
 
     return (
         <div className="lobby">
-             <SearchCourse />
+             <SearchCourse rateVal ={props.rateVal}  currencyVal={props.currencyVal}/>
         <div className="course-add">
-           <CourseCreate />
+           <CourseCreate rateVal ={props.rateVal}  currencyVal={props.currencyVal}/>
         </div>
-        <div className="instructor-list"></div>
+        {/* <div className="instructor-list">
                         <ViewProfileButton />
-            <p> </p>
+                        </div> */}
+    
         </div>
         
     )
