a80cff478adc638bd4340fe30659b30fb87e39b8
Ahmed Khaled <eng.ahmed.khaled15@gmail.com>
2022-12-18 23:15:52 +0200

    Revert "Minor Bug Fixes"
    This reverts commit 0d0c24b62eb3ee75033eed1aa2295c3ec6592aaf.

--- backend/Routes/Trainee.js
+++ backend/Routes/Trainee.js
@@ -37,12 +37,12 @@ postCourseRegister);
 router.post('/findgrade/:id',
 passport.authenticate('jwt', { session: false }),
 inRole(ROLES.TRAINEE, ROLES.ADMIN),
-findExercisesGrade);
+postCourseGrade);
 
 router.post('/findtestgrade/:id',
 passport.authenticate('jwt', { session: false }),
 inRole(ROLES.TRAINEE, ROLES.ADMIN),
-findExamGrade);
+findOldGrade);
 
 router.post('/forgot/:id',
 passport.authenticate('jwt', { session: false }),
@@ -57,66 +57,6 @@ postCreditCard);
 router.post('/checkregister/:id',
 passport.authenticate('jwt', { session: false }),
 inRole(ROLES.TRAINEE, ROLES.ADMIN),
-checkRegistered);
-
-router.post('/updatebalance/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateTraineeBalance);
-
-router.post('/updateprogress/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateCourseProgress);
-
-router.post('/courseprogress/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-checkCourseProgress);
-
-router.post('/updateexamstatus/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateSolvedExam);
-
-router.post('/updateexamgrade/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateExamGrade);
-
-router.post('/updateexercisesgrade/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateExercisesGrade);
-
-router.post('/updateexercisesstatus/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-updateSolvedExercises);
-
-router.post('/refund/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-refundCourse);
-
-router.post('/checkstatus/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-checkSolvingStatus);
-
-router.post('/checkexstatus/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-checkExercisesSolvingStatus);
-
-router.post('/findcreditcard/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
-findCreditCard);
-
-router.post('/deletecard/:id', 
-passport.authenticate('jwt', { session: false }),
-inRole(ROLES.TRAINEE),
 checkRegistered);
 
 router.post('/updatebalance/:id', 
